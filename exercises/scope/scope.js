bc370cf115b9e00a0e39758904f780198b71b5af
cyr <cyril.quandalle@gmail.com>
2017-09-16 19:18:42 +0200

    add linter

--- exercises/scope/scope.js
+++ exercises/scope/scope.js
@@ -1,14 +1,33 @@
 var c = 3
 export function display (a) {
-    var b = a * 2
-    function print(c) {
-        return [a, b, c]
-    }
-   return print(b * 3)
+  var b = a * 2
+  function print (c) {
+    return [a, b, c]
+  }
+  return print(b * 3)
 }
 
 var name = 'jon'
 export function sayMyName () {
-    var name = 'toto'
-    return name
+  var name = 'toto'
+  return name
 }
+
+export function misteriousNumber () {
+  console.log(a)
+  console.log(foo())
+
+  var a = 1
+  function foo () {
+    return 2
+  }
+}
+
+var a = 1
+function b () {
+  a = 10
+
+  function a () {}
+}
+b()
+console.log(a)
--- exercises/scope/scope.spec.js
+++ exercises/scope/scope.spec.js
@@ -1,14 +1,15 @@
-import { display } from './scope.js'
-import { sayMyName } from "./scope";
+import {misteriousNumber, display, sayMyName} from './scope'
 
-describe ('scope exercises', () => {
-    it ('a function has access to its own local scope variables', () => {
-        expect(sayMyName()).toEqual('???')
-    })
+describe('scope exercises', () => {
+  it('a function has access to its own local scope variables', () => {
+    expect(sayMyName()).toEqual('???')
+  })
 
-    it('should diplay the right number', () => {
-        expect(display(2)).toEqual('???')
-    })
+  it('should diplay the right number', () => {
+    expect(display(2)).toEqual('???')
+  })
 
+  it('should display the misterious nmber', () => {
+    misteriousNumber()
+  })
 })
-
