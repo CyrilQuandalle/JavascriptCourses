a829a1b7d0eade19d7c845d8cc6a6aa38c5350e2
cyr62 <shakersandyacht@hotmail.com>
2017-09-16 17:52:37 +0200

    first commit

---
+++ exercises/scope/scope.spec.js
@@ -0,0 +1,20 @@
+import display from './exo.js'
+describe('scope exercises', () => {
+// it('a function has access to its own local scope variables', () => {
+//     var fn = function () {
+//       var name = 'inner'
+//       ACTUAL = name
+//     }
+//     fn()
+//     expect(ACTUAL === '???').toBe(true)
+//   })
+
+  it('', () => {
+  var c = 3
+
+  console.log(display(2))
+
+})
+
+})
+
