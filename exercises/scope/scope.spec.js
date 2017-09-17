7f4168a1214d27249061ece2e5d1ae82c8d3b982
cyr62 <shakersandyacht@hotmail.com>
2017-09-17 20:02:18 +0200

    little renaming

--- exercises/scope/scope.spec.js
+++ exercises/scope/scope.spec.js
@@ -1,4 +1,4 @@
-import { display, sayMyName } from './scope'
+import { display, sayMyName } from './functionScope'
 
 describe('scope exercises', () => {
   it('a function has access to its own local scope variables', () => {
